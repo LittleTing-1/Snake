de314a22b7370f562ace0d7e19cc2020ae63dfd5
Little-ting1 <lindenlucas_8@outlook.com>
2022-04-02 18:19:48 +0100

    misc bugfixes

--- source/main.py
+++ source/main.py
@@ -1,6 +1,8 @@
 # import required libraries
-from tkinter import *
 from random import randint
+from tkinter.font import Font
+from tkinter.tix import Tk
+from tkinter.ttk import Button, Label
 
 
 class Game:
@@ -15,7 +17,11 @@ class Game:
     gameStarted = False
     gameOver = False
     highScore = 0
+    myFont = None
 
+    fontSize = 16
+    fontWeight = "normal"
+    fontFamily = "ubuntu"
     boardSize = 8
     speed = 500
     initLength = 3
@@ -29,6 +35,7 @@ class Game:
         master.bind('d', self.d)
         # begin game loop
         self.gameLoop(master)
+        self.myFont = Font(family=self.fontFamily, size=self.fontSize)
 
     # gets all widgets in inputted tkinter window and returns them in a list
     def allChildren(self, master):
@@ -40,10 +47,8 @@ class Game:
                 list.extend(item.winfo_children())
         return list
 
-    # TODO change func name to be more clear
     # loops the snake to the other side of the screen if it hits one edge
-
-    def loop(self):
+    def loopToOtherSideOfScreen(self):
         if self.headX > self.boardSize-1:
             self.headX = 0
         if self.headY > self.boardSize-1:
@@ -54,13 +59,11 @@ class Game:
             self.headY = self.boardSize-1
 
     # deletes body x and y at index 0
-
     def delBody(self):
         del self.bodyPeicesX[0]
         del self.bodyPeicesY[0]
 
     # calculates new head pos based on input dir
-
     def updateHeadPos(self):
         # adds current head pos to body pos list before moving
         self.bodyPeicesX.append(self.headX)
@@ -68,8 +71,8 @@ class Game:
         # adds dir to body pos
         self.headX += self.dirX
         self.headY += self.dirY
-        # calls loop() to loop snake around edge of the screen if head pos exceeds board size
-        self.loop()
+        # calls loopToOtherSideOfScreen() to loop snake around edge of the screen if head pos exceeds board size
+        self.loopToOtherSideOfScreen()
         if self.counter > self.initLength-1:
             if self.headX == self.fruitX and self.headY == self.fruitY:
                 self.fruit = False
@@ -97,18 +100,22 @@ class Game:
             for j in range(self.boardSize):
                 Label1 = Label(master, text=board[i][j])
                 Label1.grid(row=i, column=j)
+                Label1.configure(font=self.myFont)
 
     # generates a new fruit pos if fruit is not currently present and stores it in global vars fruitX and fruitY
     def genFruit(self):
-        # TODO stop fruit generating within body or at head pos
-        # * for graphics, rotate head based on dir
         if not self.fruit:
             self.fruitX = randint(0, self.boardSize-1)
             self.fruitY = randint(0, self.boardSize-1)
-        self. fruit = True
+        if self.fruitX == self.headX and self.fruitY == self.headY:
+            self.genFruit()
+        for i in range(len(self.bodyPeicesX)):
+            if self.fruitX == self.bodyPeicesX[i] and self.fruitY == self.bodyPeicesY[i]:
+                self.genFruit()
+        self.fruit = True
 
     # generates board to be rendered by displayBoard() - this func is the root of all the snake logic and rendering
-    # TODO optimize func to use less iterations - just iterate through boyPieces and add them to board
+    # TODO optimize func to use less iterations - just iterate through bodyPieces and add them to board
     def updateBoard(self, master):
         # self explanetory, updates head pos with updateHeadPos()
         self.updateHeadPos()
@@ -116,7 +123,7 @@ class Game:
             if self.bodyPeicesX[i] == self.headX and self.bodyPeicesY[i] == self.headY and self.gameStarted:
                 self.gameOver = True
         # create a array with size equal to var boardSize
-        board = [[" "]*self.boardSize for i in range(self.boardSize)]
+        board = [["   "]*self.boardSize for i in range(self.boardSize)]
         # calls genFruit() to get a new fruit pos and adds it to board
         self.genFruit()
         board[self.fruitX][self.fruitY] = "F"
@@ -161,8 +168,8 @@ class Game:
         Label(master, text="Your highscore is: " +
               str(self.highScore)).grid(row=3, column=0)
         self.bodyPeicesX = []
-        # TODO add restart button
-        master.after(3000, lambda: self.gameLoop(master))
+        Button(master, text="Play again", command=lambda: self.gameLoop(
+            master)).grid(row=5, column=0)
 
     # funcs that are bound to input w, a, s, and d
     def a(self, _event=None):
